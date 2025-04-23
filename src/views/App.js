d6dd57ea4b87f9337e5523b14453e4897642eda4
lappp <lap01288399558@gmail.com>
2025-04-24 02:12:19 +0700

    24.4.25

--- src/views/App.js
+++ src/views/App.js
@@ -1,35 +1,46 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import MyComponent from "./Example/MyComponent";
-import ListTodos from "./Todos/ListTodos";
-import React from "react";
+import MyComponent from "./Example/MyComponent.js";
+import ListTodo from "./Todos/ListTodos";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ListUser from "./User/ListUser";
 import Nav from "./Nav/Nav";
 import Home from "./Example/Home";
+import DetailUser from "./User/DetailUser";
+
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import ListUser from "./User/ListUser";
+
+/**
+ * 2 components: class component / function component ( function, arrow)
+ * JSX
+ */
+
 function App() {
+  // const  App = () =>  {
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <Nav />
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <MyComponent /> */}
-          {/* <ListTodos /> */}
-          {/* <Home /> */}
           <Switch>
             <Route path="/" exact>
               <Home />
             </Route>
             <Route path="/todo">
-              <ListTodos />
+              <ListTodo />
             </Route>
             <Route path="/about">
               <MyComponent />
             </Route>
-            <Route path="/user">
+
+            <Route path="/user" exact>
               <ListUser />
+            </Route>
+
+            <Route path="/user/:id">
+              <DetailUser />
             </Route>
           </Switch>
         </header>
@@ -39,12 +50,11 @@ function App() {
           autoClose={5000}
           hideProgressBar={false}
           newestOnTop={false}
-          closeOnClick={false}
+          closeOnClick
           rtl={false}
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="light"
         />
       </div>
     </BrowserRouter>
--- src/views/User/ListUser.js
+++ src/views/User/ListUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "./ListUser.scss";
+import { withRouter } from "react-router-dom";
 class ListUser extends React.Component {
   state = {
     listUsers: [],
@@ -12,6 +13,10 @@ class ListUser extends React.Component {
     });
   }
 
+  handleViewDetailUser = (user) => {
+    this.props.history.push(`/user/${user.id}`);
+  };
+
   render() {
     let { listUsers } = this.state;
     return (
@@ -22,7 +27,11 @@ class ListUser extends React.Component {
             listUsers.length > 0 &&
             listUsers.map((item, index) => {
               return (
-                <div className="child" key={item.id}>
+                <div
+                  className="child"
+                  key={item.id}
+                  onClick={() => this.handleViewDetailUser(item)}
+                >
                   {index + 1} - {item.first_name} - {item.last_name}
                 </div>
               );
@@ -32,4 +41,4 @@ class ListUser extends React.Component {
     );
   }
 }
-export default ListUser;
+export default withRouter(ListUser);
